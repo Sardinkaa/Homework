91f16ed1a4642ced357e1cec17b04ad6b1e908c6
Селиверстова Серафима 1МД17 <99961685+Sardinkaa@users.noreply.github.com>
2022-02-25 13:27:56 +0300

    Add files via upload

---
+++ Селиверстова.py
@@ -0,0 +1,77 @@
+# #n1
+# n = "Сима"
+# print(n)
+#n2
+# age = 18
+# n = "Сима"
+# print("Я", n, "и мне", age, "лет")
+#n3
+# name = "Сима"
+# n = name+name+name+name+name
+# print(n)
+#n4
+# print("Как тебя зовут?")
+# n = input()
+# print("Сколько тебе лет?")
+# age = input()
+# print("Привет,", n + "!", "Ну ты и старпер!")
+#n5
+# print("Сколько тебе лет?")
+# age = int(input())
+# if age <= 5:
+#     print("Да ты еще даже читать не умеешь!")
+# if (age >=6) and (age <= 17):
+#     print("Сначала школу окончи")
+# if (age >= 18) and (age <= 55):
+#     print("Работа - шутка для тебя?")
+# if age >= 56:
+#     print("Ого! Какой же ты старый!")
+#n6
+# print("Как тебя зовут?")
+# n = input()
+# k = " "
+# m = " "
+# for i in range(1,len(n)-1):
+#     k = k + n[i]
+# print(k)
+# p = n[::-1]
+# print(p)
+# l = n[len(n)-3] + n[len(n)-2] + n[len(n)-1]
+# print(l)
+# m = n[0] + n[1] + n[2] + n[3] + n[4]
+# print(m)
+#n7
+# n = input()
+# age = int(input())
+# l = len(n)
+# a = age // 10
+# b = age % 10
+# print(l,a+b,a*b)
+#n8
+# n = input()
+# lowerstring = n.lower()
+# print(lowerstring)
+# upperstring = n.upper()
+# print(upperstring)
+# capitalize = n.capitalize()
+# print(capitalize)
+# c = capitalize.swapcase()
+# print(c)
+#n9
+# print("веди имя")
+# n = input()
+# print("введи возраст")
+# age = input()
+# k = " "
+# for i in range((len(n)-1)):
+#     if n[i] == k
+#         print("ты читать не умеешь?")
+#     if (a < 0) or (a > 150)
+#         print("не ври")
+#n10
+# print("Реши задачку: 7 * 5 + 9")
+# k = input()
+# if k != 44:
+#     print("лошара")
+# else:
+#     print("красава")
